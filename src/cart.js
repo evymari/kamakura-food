2401b1cd48cd922d0966a02c6873ac834c13e0af
pilimuino <pili.muino@gmail.com>
2024-06-05 14:38:07 +0200

    primer cambio añadido

--- src/cart.js
+++ src/cart.js
@@ -1,2 +1,19 @@
 //DEBE contener las funcionalidades del carrito de compras.
 
+import { filters, products } from "../src/assets/data/data.js";
+
+// funcion para no añadir dos veces el mismo id
+
+window.addToCart = function(product) {
+    const existingProduct = cart.find(item => item.id === product.id);
+    if (existingProduct) {
+        existingProduct.quantity += 1;
+    } else {
+        cart.push({...product, quantity: 1 });
+    }
+    updateCart();
+};
+
+
+
+
