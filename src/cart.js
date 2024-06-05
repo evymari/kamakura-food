fdd8d164de6c1eae12d10564a05b0b1963d17199
indiakka <78439130+indiakka@user.noreply.github.com>
2024-06-05 15:01:30 +0200

    funcionalidad añadir carrito

--- src/cart.js
+++ src/cart.js
@@ -1,2 +1,36 @@
 //DEBE contener las funcionalidades del carrito de compras.
+const cart = document.querySelector(".cart-container");
+const cartList = cart.querySelector(".cart-products");
+let cartItems = [];
 
+document
+  .getElementsByClassName("add-button")
+  .addEventListener("click", (event) => {
+    productList.addEventListener("click", addProduct);
+  });
+
+function addProduct(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("add-button")) {
+    selectedProduct = e.target.parentElement.parentElement;
+    getCartData(selectedProduct);
+  }
+}
+
+function cart() {
+  cartItems.forEach((product) => {
+    const row = document.createElement(".cart-container");
+
+    row.innerHTML = `
+       <h3>${product.name}</h3>
+       <h5> ${product.price}</h5>
+       `;
+  });
+
+  cartList.appendChild(row);
+}
+function clearCart() {
+  while (cartList.firstChild) {
+    cartList.removeChild(cartList.firstChild);
+  }
+}
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,17 @@
 //DEBE imprimir en pantalla la información de filtros.
 
+import { products, filters} from "../src/assets/data/data.js"
+  function printPlates(products){
+    let result = products.map(item => item.products);
+   
+  console.log(result);
+  return result;
+};
+
+
+window.onload = function () {
+  printPlates();
+};
+
+
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
