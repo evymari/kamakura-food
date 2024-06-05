50eb1d7ce0ebf7eb569c58b58eb6d038b66c52b4
Seda07 <gevorgyanseda0@gmail.com>
2024-06-05 14:17:48 +0200

    se sube los cambios

--- src/events.js
+++ src/events.js
@@ -1,2 +1,12 @@
 //Intenta separar los eventos en este archivo.
 
+// abrir y cerrar un menú lateral
+
+document.getElementById("cart").addEventListener("click", function() {
+   const cartContainer = document.getElementById("cart-container"); 
+   if (cartContainer.style.display === "none" ||  cartContainer.style.display === "") {
+    cartContainer.style.display = "block";
+   } else {
+    cartContainer.style.display = "none" ;
+   }
+});
