0bcd9ce3b43c0a040c453e3f8c07c57d09b52090
pilimuino <pili.muino@gmail.com>
2024-06-05 16:59:59 +0200

    cambios añadidos

--- src/cart.js
+++ src/cart.js
@@ -3,17 +3,19 @@
 import { filters, products } from "../src/assets/data/data.js";
 
 // funcion para no añadir dos veces el mismo id
-
-window.addToCart = function(product) {
-    const existingProduct = cart.find(item => item.id === product.id);
+ function addToCart(product) {
+   const existingProduct = cart.find(item => item.id === product.id);
     if (existingProduct) {
         existingProduct.quantity += 1;
     } else {
         cart.push({...product, quantity: 1 });
     }
+   updateCart();
+
+
+//funcion para eliminar productos del carrito
+function removeFromCart(productId) {
+    cart = cart.filter(item =>item.id !==productId);
     updateCart();
 };
-
-
-
-
+ }
